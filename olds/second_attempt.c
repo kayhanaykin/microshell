3bd03d009cede3df0eb271900800e62ed29dc82d
Kayhan Aykin <kaykin@k1m26s04.42istanbul.com.tr>
2024-11-10 16:30:57 +0300

    unwanted error responses

--- olds/second_attempt.c
+++ olds/second_attempt.c
@@ -11,14 +11,15 @@ void	close_pipes(int *fd1, int *fd2);
 void	create_cmd();
 void	exec_cd();
 void	wait_pid();
-
+int		ft_strlen(char *str);
+void	exit_handler(char *str, char *str2, int no);
 
 int 			i = 1;
 int				start = 1;
 int 			cmd_count = 0;
 int				fd1[2];
 int				fd2[2];
-int 			pid;
+int 			pid = 0;
 extern	char 	**environ;
 char			**cmd;
 char			**arg_ptr;
@@ -32,6 +33,7 @@ int main(int ac, char *args[])
 		if (strcmp(args[i], ";") == 0)
 		{
 			exec_out();
+			wait_pid();
 		}
 		if (strcmp(args[i], "|") == 0)
 		{
@@ -40,6 +42,7 @@ int main(int ac, char *args[])
 		if (strcmp(args[i], "cd") == 0)
 		{
 			exec_cd();
+			wait_pid();
 		}
 		i++;
 	}
@@ -49,13 +52,18 @@ int main(int ac, char *args[])
 
 void exec_first()
 {
-	pipe(fd1);
+	if (pipe(fd1) == -1)
+		exit_handler("error: 1fatal", NULL, errno);
 	pid = fork();
+	if (pid == -1)
+		exit_handler("error: 2fatal", NULL, errno);
 	if (pid == 0)
 	{
-		dup2(fd1[1], STDOUT_FILENO);
+		if (dup2(fd1[1], STDOUT_FILENO) == -1)
+			exit_handler("error: 3fatal", NULL, errno);
 		close_pipes(fd1, NULL);
-		execve(cmd[0], cmd, environ);
+		if (execve(cmd[0], cmd, environ) == -1)
+			exit_handler("error: cannot execute ", cmd[0], 0);
 	}
 }
 
@@ -68,24 +76,38 @@ void exec()
 		exec_first();
 	else if (cmd_count % 2 == 1)
 	{		
-		pipe(fd1);
+		if (pipe(fd1) == -1)
+			exit_handler("error: 4fatal", NULL, errno);
 		pid = fork();
+		if (pid == -1)
+			exit_handler("error: 5fatal", NULL, errno);
 		if (pid == 0)
 		{
-			dup2(fd2[0], STDIN_FILENO);
-			dup2(fd1[1], STDOUT_FILENO);
+			if (dup2(fd2[0], STDIN_FILENO) == -1)
+				exit_handler("error: 6fatal", NULL, errno);
+			if (dup2(fd1[1], STDOUT_FILENO) == -1)
+				exit_handler("error: 7fatal", NULL, errno);
+			if (execve(cmd[0], cmd, environ) == -1)
+				exit_handler("error: cannot execute ", cmd[0], 0);
 			close_pipes(fd1, fd2);
 		}
 		close_pipes(NULL, fd2);
 	}
 	else
 	{
-		pipe(fd2);
+		if (pipe(fd2) == -1)
+			exit_handler("error: 8fatal", NULL, errno);
 		pid = fork();
+		if (pid == -1)
+			exit_handler("error: 9fatal", NULL, errno);
 		if (pid == 0)
 		{
-			dup2(fd1[0], STDIN_FILENO);
-			dup2(fd2[1], STDOUT_FILENO);
+			if (dup2(fd1[0], STDIN_FILENO) == -1)
+				exit_handler("error: 10fatal", NULL, errno);
+			if (dup2(fd2[1], STDOUT_FILENO) == -1)
+				exit_handler("error: 11fatal", NULL, errno);
+			if (execve(cmd[0], cmd, environ) == -1)
+				exit_handler("error: cannot execute ", cmd[0], 0);
 			close_pipes(fd1, fd2);
 		}
 		close_pipes(fd1, NULL);
@@ -100,41 +122,56 @@ void	exec_out()
 	else if (cmd_count == 1)
 	{
 		pid = fork();
+		if (pid == -1)
+			exit_handler("error: 12fatal", NULL, errno);
 		if (pid == 0)
 		{
-			execve(cmd[0], cmd, environ);
+			if (execve(cmd[0], cmd, environ) == -1)
+				exit_handler("error: cannot execute ", cmd[0], 0);
 		}
 	}
 	else if (cmd_count == 2)
 	{
 		pid = fork();
+		if (pid == -1)
+			exit_handler("error: 13fatal", NULL, errno);
 		if (pid == 0)
 		{
-			dup2(fd1[0], STDIN_FILENO);
+			if (dup2(fd1[0], STDIN_FILENO) == -1)
+				exit_handler("error: 14fatal", NULL, errno);
 			close_pipes (fd1, NULL);
-			execve(cmd[0], cmd, environ);
+			if(execve(cmd[0], cmd, environ) == -1)
+				exit_handler("error: cannot execute ", cmd[0], 0);
 		}
 		close_pipes(fd1, NULL);
 	}
 	else if (cmd_count % 2 == 1)
 	{
 		pid = fork();
+		if (pid == -1)
+			exit_handler("error: 14fatal", NULL, errno);
 		if (pid == 0)
 		{
-			dup2(fd2[0], STDIN_FILENO);
+			if (dup2(fd2[0], STDIN_FILENO) == -1)
+				exit_handler("error: 15fatal", NULL, errno);
 			close_pipes (NULL, fd2);
-			execve(cmd[0], cmd, environ);
+			if (execve(cmd[0], cmd, environ) == -1)
+				exit_handler("error: cannot execute ", cmd[0], 0);
 		}
 		close_pipes(NULL, fd2);
 	}
 	else
 	{
 		pid = fork();
+		if (pid == -1)
+			exit_handler("error: 16fatal", NULL, errno);
 		if (pid == 0)
 		{
-			dup2(fd1[0], STDIN_FILENO);
+			if (dup2(fd1[0], STDIN_FILENO) == -1)
+				exit_handler("error: 17fatal", NULL, errno);
 			close_pipes (fd1, NULL);
-			execve(cmd[0], cmd, environ);
+			if (execve(cmd[0], cmd, environ) == -1)
+				exit_handler("error: cannot execute ", cmd[0], 0);
 		}
 		close_pipes(fd1, NULL);
 	}
@@ -144,20 +181,30 @@ void	close_pipes(int *fd1, int *fd2)
 {
 	if (fd1)
 	{
-		close(fd1[0]);
-		close(fd1[1]);
+		if (close(fd1[0]) == -1)
+			exit_handler("error: 18fatal", NULL, errno);
+		if (close(fd1[1]) == -1)
+			exit_handler("error: 19fatal", NULL, errno);
 	}
 	if (fd2)
 	{
-		close(fd2[0]);
-		close(fd2[1]);
+		if (close(fd2[0]) == -1)
+			exit_handler("error: 20fatal", NULL, errno);
+		if (close(fd2[1]) == -1)
+			exit_handler("error: 21fatal", NULL, errno);
 	}
 }
 
 void	wait_pid()
 {
+	int d;
+	if (pid == 0)
+		return ;
 	while (errno != ECHILD)
-		waitpid(-1, NULL, 0);
+	{
+		d = waitpid(-1, NULL, 0) 
+			exit_handler("error: 22fatal", NULL, errno);
+	}
 }
 
 void	create_cmd()
@@ -181,16 +228,53 @@ void	exec_cd()
 {
 	int j = 0;
 
-	j = i + 1;
-	while ((arg_ptr[j]) && strcmp(arg_ptr[j], ";"))
-		j++;
-	j -= (i + 1);
-	if (j != 1)
+	pid = fork();
+	if (pid == -1)
+		exit_handler("error: 23fatal", NULL, errno);
+	if (pid == 0)
 	{
-		write (2, "error: cd: bad arguments\n", 25);
-		return ;
+		j = i + 1;
+		while ((arg_ptr[j]) && strcmp(arg_ptr[j], ";"))
+			j++;
+		j -= (i + 1);
+		if (j != 1)
+		{
+			if (write (2, "error: cd: bad arguments\n", 25) == -1)
+				exit_handler("error: 24fatal", NULL, 0);
+			return ;
+		}
+		if (i != 1 && strcmp(arg_ptr[i - 1], ";") != 0)
+			return ;
+		if (chdir(arg_ptr[j + 1]) == -1)
+		{
+			exit_handler("error: cd: cannot change directory to ", arg_ptr[j + 1], 0);
+			if (write(2, "\n", 1) == -1)
+				exit_handler("error: 25fatal", NULL, 0);
+		}
 	}
-	if (i != 1 || strcmp(arg_ptr[i - 1], ";") != 0)
-		return ;
-	chdir(arg_ptr[j + 1]);
+}
+
+int	ft_strlen(char *str)
+{
+	int j = 0;
+
+	while(str[j])
+	{
+		j++;
+	}
+	return (j);
+}
+
+void	exit_handler(char *str, char *str2, int no)
+{
+	if (str)
+		if (write(2, str, ft_strlen(str)) == -1)
+			exit_handler("error: 26fatal", NULL, errno);
+	if (str2)
+		if (write(2, str2, ft_strlen(str2)) == -1)
+			exit_handler("error: 27fatal", NULL, errno);
+	if (write(2, "\n", 1) == -1)
+		exit_handler("error: 28fatal", NULL, errno);
+	if (no)
+		exit(no);
 }
